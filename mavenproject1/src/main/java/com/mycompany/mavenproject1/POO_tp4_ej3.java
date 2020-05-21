31e48bc72b4a34d5c6f5ae6d337b375c452bf8a2
jccol <jccol@192.168.0.132>
2020-05-20 23:26:42 -0300

    comit clase main

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/POO_tp4_ej3.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jccol
+ */
+public class POO_tp4_ej3 {
+    public static void calcular_total(LinkedList<Producto>lista){
+        double total=0;
+        System.out.println("------------------------------------");
+        for(int i=0;i<lista.size();i++){
+            System.out.println("Cod producto: "+lista.get(i).get_codigo()+" | Precio: "+lista.get(i).get_precio());
+            total=total+lista.get(i).get_precio();
+        }
+        System.out.println("------------------------------------");
+        System.out.println("Total a pagar: $"+total);
+        System.out.println("------------------------------------");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Producto prod=new Producto();
+        LinkedList<Producto>lista=new LinkedList<>();
+        Scanner entrada=new Scanner(System.in);
+        String opcion="";
+        boolean opcion1=true;
+        while(!"n".equals(opcion)){
+            System.out.println("Ingrese codigo de producto: ");
+            prod.set_codigo(entrada.nextInt());
+            System.out.println("Ingrese precio de producto: ");
+            prod.set_precio(entrada.nextDouble());
+            System.out.println("¿Desea agregar otro producto?(s/n)");
+            opcion=entrada.next();
+            lista.add(prod);
+            prod=new Producto();
+        }
+        calcular_total(lista);
+    }
+}
+    
+
